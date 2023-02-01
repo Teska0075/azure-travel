9a546d038477903b9eecf46d655f341019b7e8a7
Evening <evening@Admins-iMac.local>
2023-02-01 18:12:40 +0800

    qwe

--- src/components/Hero/Navigation/NavBar.jsx
+++ src/components/Hero/Navigation/NavBar.jsx
@@ -2,17 +2,36 @@ import * as React from "react";
 import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import BasicModal from "../Modal";
+import {
+  Alert,
+  IconButton,
+  Menu,
+  MenuItem,
+  Snackbar,
+  Tooltip,
+  Avatar,
+} from "@mui/material";
 
-const drawerWidth = 240;
 const navItems = ["Home", "Stays", "Flights", "Packages"];
 
 function DrawerAppBar(props) {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
@@ -54,6 +73,35 @@ function DrawerAppBar(props) {
             ))}
           </Box>
           <BasicModal />
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
--- src/pages/Authentication/SignIn/SignIn.jsx
+++ src/pages/Authentication/SignIn/SignIn.jsx
@@ -11,17 +11,18 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { useState } from "react";
-import { Alert, Snackbar } from "@mui/material";
+
 import { useNavigate } from "react-router-dom";
+import { Alert, Snackbar } from "@mui/material";
 
 const SignIn = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
-    // const data = new FormData(event.currentTarget);
-    // console.log({
-    //   email: data.get("email"),
-    //   password: data.get("password"),
-    // });
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
   };
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -131,7 +132,7 @@ const SignIn = () => {
         anchorOrigin={{ vertical: "top", horizontal: "left" }}
       >
         <Alert severity="error" sx={{ width: "100%" }}>
-          Username or Password is incorrect or empty !
+          Username or Password is incorrect or empty.
         </Alert>
       </Snackbar>
     </Container>
